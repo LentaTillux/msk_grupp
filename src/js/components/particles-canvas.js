0f6860a0384715c4b31dde46f12830affc1b6437
NazariyM <nazarriko@gmail.com>
2017-07-27 16:07:06 +0300

    upd

--- src/js/components/particles-canvas.js
+++ src/js/components/particles-canvas.js
@@ -2,19 +2,36 @@ import particles from 'particles.js';
 import { detectDevice } from '../helpers';
 import Parallax from '../lib/parallax.min';
 
-let detectMob = detectDevice(767);
+const detectMob = detectDevice(767);
 
-initParticles();
-function initParticles() {
+// particles init
+(function () {
+  const aquaParticles = $('#js-aqua-particles').length;
+  const blueParticles = $('#js-blue-particles').length;
+  const grayParticles = $('#js-gray-particles').length;
+
   if (!detectMob) {
-    particlesJS.load('js-aqua-particles', 'assets/aqua-particles.json');
-    particlesJS.load('js-blue-particles', 'assets/blue-particles.json');
-    particlesJS.load('js-gray-particles', 'assets/gray-particles.json');
+    if (aquaParticles) {
+      particlesJS.load('js-aqua-particles', 'assets/aqua-particles.json');
+    }
+    if (blueParticles) {
+      particlesJS.load('js-blue-particles', 'assets/blue-particles.json');
+    }
+    if (grayParticles) {
+      particlesJS.load('js-gray-particles', 'assets/gray-particles.json');
+    }
   }
-}
+})();
 
-const whyUsParallax = $('#js-why-us-parallax').get(0);
-const parallax = new Parallax(whyUsParallax);
+// parallax init
+(function () {
+  if ($('#js-why-us-parallax').length) {
+    const whyUsParallax = $('#js-why-us-parallax').get(0);
+    const parallax = new Parallax(whyUsParallax);
+  }
 
-const scrParallax = $('#js-screen-parallax').get(0);
-const screenParallax = new Parallax(scrParallax);
+  if ($('#js-screen-parallax').length) {
+    const scrParallax = $('#js-screen-parallax').get(0);
+    const screenParallax = new Parallax(scrParallax);
+  }
+})();
