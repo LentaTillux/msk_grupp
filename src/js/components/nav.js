91b069d4f0d288b7f922facf46e8d5631967b017
NazariyM <nazarriko@gmail.com>
2017-08-01 09:04:48 +0300

    upd

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -3,10 +3,12 @@ import { BODY, WINDOW } from '../dev/helpers';
 (function () {
   const $nav = $('.js-nav');
   const $navBtn = $('.js-nav-toggle');
+  const $navLogo = $('.js-logo');
 
   $navBtn.on('click', function (e) {
     const $this = $(this);
     $this.toggleClass('active');
+    $navLogo.toggleClass('active');
     BODY.toggleClass('is-locked');
     $nav.slideToggle().css('display', 'flex');
   });
